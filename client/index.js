06174bf62e81de4b55eda78754fc38c36f1d5684
do7be <bomsap700@gmail.com>
2017-05-07 19:05:19 +0900

    auto resolve chromeID

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 function openTab() {
-  var callback = chrome.extension.getURL("oauth_callback.html")
-  chrome.tabs.create({ 'url': 'https://shrouded-ravine-50926.herokuapp.com/oauth/authorize?client_id=1&redirect_uri=chrome-extension://ohmmemmhjcnccpghiachpikplacnkmid/oauth_callback.html&response_type=token' });
+  var callbackURL = chrome.extension.getURL("oauth_callback.html")
+  chrome.tabs.create({ 'url': `https://shrouded-ravine-50926.herokuapp.com/oauth/authorize?client_id=1&redirect_uri=${callbackURL}&response_type=token` });
 }
 
 chrome.browserAction.onClicked.addListener(openTab);
